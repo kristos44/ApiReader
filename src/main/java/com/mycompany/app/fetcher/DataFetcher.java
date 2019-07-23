fa8a786b767e772bcbd849711d1dca115f0a95bb
Krzysztof Tkacz <kristos44@gmail.com>
2019-07-23 11:25:53 +0200

    ResponseFormatter class added.

--- src/main/java/com/mycompany/app/fetcher/DataFetcher.java
+++ src/main/java/com/mycompany/app/fetcher/DataFetcher.java
@@ -1,5 +1,6 @@
 package com.mycompany.app.fetcher;
 
+import com.mycompany.app.formatter.ResponseFormatter;
 import com.mycompany.app.integration.FuelApiClient;
 import com.mycompany.app.mapper.JsonToModelMapper;
 import com.mycompany.app.model.StationData;
@@ -10,15 +11,18 @@ import java.util.*;
 public class DataFetcher {
     private FuelApiClient fuelApiClient;
     private JsonToModelMapper jsonToModelMapper;
+    private ResponseFormatter responseFormatter;
 
     public DataFetcher() {
         fuelApiClient = new FuelApiClient();
         jsonToModelMapper = new JsonToModelMapper();
+        responseFormatter = new ResponseFormatter();
     }
 
     public DataFetcher(FuelApiClient fuelApiClient) {
         this.fuelApiClient = fuelApiClient;
         jsonToModelMapper = new JsonToModelMapper();
+        responseFormatter = new ResponseFormatter();
     }
 
     public String fetchData(String apiUrl, String country) {
@@ -30,49 +34,6 @@ public class DataFetcher {
         Optional<StationData> dynamicStationDataOptional = jsonToModelMapper.mapJsontoStationData(
                 fuelApiClient.fetchStationData(apiUrl + properties.get("dynamicApiUrl") + country));
 
-        Map<String, Integer> stationsStats = new HashMap<>();
-
-        if(staticStationDataOptional.isPresent() && dynamicStationDataOptional.isPresent()) {
-            StationData staticStationData = staticStationDataOptional.get();
-            StationData dynamicStationData = dynamicStationDataOptional.get();
-
-            staticStationData.getStationMap().forEach((k, v) -> {
-                if(dynamicStationData.getStationMap().containsKey(k)) {
-                    String brand = v.getBrand();
-                    if(stationsStats.containsKey(brand)) {
-                        stationsStats.put(brand, stationsStats.get(brand) + 1);
-                    } else {
-                        stationsStats.put(brand, 1);
-                    }
-                }
-            });
-
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("Stacji ogółem: " + staticStationData.getStationMap().size() + "\n");
-            stringBuilder.append("Stacji z cenami: " + dynamicStationData.getStationMap().size() + "\n");
-
-            Map<String, Integer> sortedStationsStats = sortByValues(stationsStats);
-            sortedStationsStats.forEach((k, v) -> stringBuilder.append(k + " " + v + "\n"));
-
-            return stringBuilder.toString();
-        }
-
-        return "";
-    }
-
-    public static <K extends Comparable<K>, V extends Comparable<V>> Map<K, V> sortByValues(final Map<K, V> map) {
-        Comparator<K> valueComparator =
-                (k1, k2) -> {
-                    int compare = map.get(k2).compareTo(map.get(k1));
-                    if (compare == 0) {
-                        return k1.compareTo(k2);
-                    } else {
-                        return compare;
-                    }
-                };
-
-        Map<K, V> sortedByValues = new TreeMap<>(valueComparator);
-        sortedByValues.putAll(map);
-        return sortedByValues;
+        return responseFormatter.formatResponse(staticStationDataOptional, dynamicStationDataOptional);
     }
 }
